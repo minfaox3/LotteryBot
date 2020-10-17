e35fa9c6db10df2159362089c56bc36db55fca9c
minfaox3 <spdlci30@gmail.com>
2020-10-17 12:47:41 +0900

    [add]

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,12 @@
+import psycopg2
 import discord
 import random
 import os
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+connection = psycopg2.connect(os.environ['DATABASE_URL'])
+cursor = connection.cursor()
 
 @client.event
 async def on_ready():
@@ -12,11 +15,21 @@ async def on_ready():
 async def reply(message):
     operations = message.content.split(' ')
     content = ""
-    if operations[1] == "run":
-        if len(operations)>=2 and operations[2][0] == '@' and operations[2][1:].isdigit():
-            content = operations[2][1:]
-        elif len(operations)>=3 and operations[2].isdigit() and operations[3].isdigit():
-            content = random.randint(int(operations[2]),int(operations[3]))
+    if len(operations)>=2:
+        if operations[1] == "run":
+            if len(operations) >= 3 and operations[2][0] == '@' and operations[2][1:].isdigit():
+                content = operations[2][1:]
+            elif len(operations) >= 4 and operations[2].isdigit() and operations[3].isdigit():
+                content = random.randint(int(operations[2]), int(operations[3]))
+        elif operations[1] == "create":
+            content = ""
+        elif operations[1] == "list":
+            if len(operations) >= 3 and operations[2] == "public":
+                content = "public"
+            else:
+                cursor.execute("\\dt")
+                print(cursor)
+                content = ""
     await message.channel.send(content)
 
 @client.event
